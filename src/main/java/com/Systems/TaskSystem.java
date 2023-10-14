b4aa0dd83aed933711e97d0eaa97ae4c8c3d9dcf
Niss7z <timtsinn@gmail.com>
2023-10-14 14:47:55 +0630

    changed tasksystem classs

--- src/main/java/com/Systems/TaskSystem.java
+++ src/main/java/com/Systems/TaskSystem.java
@@ -6,8 +6,27 @@ import com.models.Task;
 import java.util.ArrayList;
 
 public class TaskSystem {
-    ArrayList<Folder> all_folders;
-    ArrayList<Task> all_tasks;
+    // this has to be the same throughout all classes
+    private static ArrayList<Folder> all_folders;
+    private static ArrayList<Task> all_tasks;
+
+    public ArrayList<Folder> getAll_folders() {
+        return all_folders;
+    }
+
+    public ArrayList<Task> getAll_tasks(){
+        return all_tasks;
+    }
+
+    // because tasks can be moved to other folders
+    public void moveTask(Task task, Folder folder){
+        task.setParent_folder(folder);
+        folder.getTasks().add(task);
+        DatabaseConnection db = new DatabaseConnection();
+        db.updateTask(task);
+        db.updateFolder(folder);
+    }
+
 
 
 }
